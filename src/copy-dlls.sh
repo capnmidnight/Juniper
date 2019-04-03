2c32c4559218e85c4ffa0e74f4f6574261465508
Sean T. McBeth <sean.mcbeth@gmail.com>
2019-04-03 16:05:50 -0400

    upgrade Accord.NET to v3.8.2

--- src/copy-dlls.sh
+++ src/copy-dlls.sh
@@ -1,6 +1,10 @@
 cp -va Juniper.Units/src/bin/* Juniper/Assets/Juniper/Plugins/Juniper
 cp -va Juniper.NeuroSky/src/bin/* Juniper/Assets/Juniper/Plugins/Juniper
 cp -va Juniper.Common/src/bin/* Juniper/Assets/Juniper/Plugins/Juniper
+
 cp -va Hjg.Pngcs/bin/* Juniper/Assets/Juniper/Plugins/Juniper
+
+cp -va accord-net-framework/Release/net46/* Juniper/Assets/Juniper/Plugins/Accord.NET
+rm Juniper/Assets/Juniper/Plugins/Accord.NET/ICSharpCode.SharpZipLib*
 
 sleep 3
