9b19e156cd640ab0f0a725b3856f5d9a1c15830c
Sean T. McBeth <sean.mcbeth@gmail.com>
2019-04-05 15:40:33 -0400

    fix metrics calculation

--- etc/run-metrics.sh
+++ etc/run-metrics.sh
@@ -1,11 +1,25 @@
 scc \
 -c -i="cs,shader" \
 --exclude-dir="\
+..\\src\\accord-net-extensions,\
+..\\src\\accord-net-framework,\
 ..\\src\\Hjg.Pngcs,\
 ..\\src\\packages,\
 ..\\src\\Juniper.Common\\tests\\obj,\
 ..\\src\\Juniper.Units\\tests\\obj,\
 ..\\src\\Juniper\\Library,\
 ..\\src\\Juniper\\Packages,\
-..\\src\\Juniper\\Assets\\TextMesh Pro"\
+..\\src\\Juniper\\Assets\\Bakery,\
+..\\src\\Juniper\\Assets\\Editor,\
+..\\src\\Juniper\\Assets\\GoogleARCore,\
+..\\src\\Juniper\\Assets\\GoogleVR,\
+..\\src\\Juniper\\Assets\\MagicLeap,\
+..\\src\\Juniper\\Assets\\Oculus,\
+..\\src\\Juniper\\Assets\\PicoMobileSDK,\
+..\\src\\Juniper\\Assets\\PlayServicesResolver,\
+..\\src\\Juniper\\Assets\\StreamingAssets,\
+..\\src\\Juniper\\Assets\\TextMesh Pro,\
+..\\src\\Juniper\\Assets\\Tobii,\
+..\\src\\Juniper\\Assets\\WaveVR,\
+..\\src\\Juniper\\Assets\\Juniper\\ThirdParty"\
     --avg-wage 110000 ../src/
