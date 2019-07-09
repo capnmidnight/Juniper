c8b33273a8b9374a54a7457be59c56c7113980a0
Sean T. McBeth <sean.mcbeth@gmail.com>
2019-07-08 23:07:02 -0400

    some cleanup

--- src/Juniper.World/GoogleMaps/Example.cs
+++ src/Juniper.World/GoogleMaps/Example.cs
@@ -2,37 +2,30 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace SignUrl
+namespace Juniper.GoogleMaps
 {
 
-    public struct GoogleSignedUrl
+    public static class Requester
     {
-
         public static string Sign(string url, string keyString)
         {
-            ASCIIEncoding encoding = new ASCIIEncoding();
-
             // converting key to bytes will throw an exception, need to replace '-' and '_' characters first.
-            string usablePrivateKey = keyString.Replace("-", "+").Replace("_", "/");
-            byte[] privateKeyBytes = Convert.FromBase64String(usablePrivateKey);
+            var usablePrivateKey = keyString.Replace("-", "+").Replace("_", "/");
+            var privateKeyBytes = Convert.FromBase64String(usablePrivateKey);
 
-            Uri uri = new Uri(url);
-            byte[] encodedPathAndQueryBytes = encoding.GetBytes(uri.LocalPath + uri.Query);
+            var uri = new Uri(url);
+            var encodedPathAndQueryBytes = Encoding.ASCII.GetBytes(uri.LocalPath + uri.Query);
 
             // compute the hash
-            HMACSHA1 algorithm = new HMACSHA1(privateKeyBytes);
-            byte[] hash = algorithm.ComputeHash(encodedPathAndQueryBytes);
+            var algorithm = new HMACSHA1(privateKeyBytes);
+            var hash = algorithm.ComputeHash(encodedPathAndQueryBytes);
 
             // convert the bytes to string and make url-safe by replacing '+' and '/' characters
-            string signature = Convert.ToBase64String(hash).Replace("+", "-").Replace("/", "_");
+            var signature = Convert.ToBase64String(hash).Replace("+", "-").Replace("/", "_");
 
             // Add the signature to the existing URI.
-            return uri.Scheme + "://" + uri.Host + uri.LocalPath + uri.Query + "&signature=" + signature;
+            return $"{uri.Scheme}://{uri.Host}{uri.LocalPath}{uri.Query}&signature={signature}";
         }
-    }
-
-    class Program
-    {
 
         static void Main()
         {
@@ -65,7 +58,7 @@ namespace SignUrl
                 inputKey = keyString;
             }
 
-            Console.WriteLine(GoogleSignedUrl.Sign(inputUrl, inputKey));
+            Console.WriteLine(Sign(inputUrl, inputKey));
         }
     }
 }
