67d85632909797f487e8e4f615f28b083c597882
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2019-07-25 16:44:26 -0400

    remove a few more directories from statistics

--- etc/run-metrics.sh
+++ etc/run-metrics.sh
@@ -2,11 +2,13 @@ scc \
 -c -i="cs,shader" \
 --exclude-dir="\
 ..\\src\\accord-net-extensions,\
-..\\src\\accord-net-framework,\
 ..\\src\\Hjg.Pngcs,\
 ..\\src\\packages,\
+..\\src\\TestResults,\
 ..\\src\\Juniper\\Library,\
 ..\\src\\Juniper\\Packages,\
+..\\src\\Juniper\\ProjectSettings,\
+..\\src\\Juniper\\QCAR,\
 ..\\src\\Juniper\\Assets\\Editor,\
 ..\\src\\Juniper\\Assets\\GoogleARCore,\
 ..\\src\\Juniper\\Assets\\GoogleVR,\
@@ -19,5 +21,5 @@ scc \
 ..\\src\\Juniper\\Assets\\Tobii,\
 ..\\src\\Juniper\\Assets\\WaveVR,\
 ..\\src\\Juniper\\Assets\\Juniper\\Plugins,\
-..\\src\\Juniper\\Assets\\ThirdParty"\
+..\\src\\Juniper\\Assets\\Juniper\\ThirdParty"\
     --avg-wage 150000 ../src/
