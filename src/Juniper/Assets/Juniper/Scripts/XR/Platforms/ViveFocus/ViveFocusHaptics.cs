bbebffe3f097339765c91f802adcb60f3255b033
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2019-09-06 16:00:48 -0400

    used the wrong time conversion here

--- src/Juniper/Assets/Juniper/Scripts/XR/Platforms/ViveFocus/ViveFocusHaptics.cs
+++ src/Juniper/Assets/Juniper/Scripts/XR/Platforms/ViveFocus/ViveFocusHaptics.cs
@@ -15,13 +15,13 @@ namespace Juniper.Haptics
         {
             var start = DatTime.Now;
             var seconds = Units.Milliseconds.Seconds(milliseconds);
-            var nanoseconds = Units.Milliseconds.Nanoseconds(milliseconds);
+            var microseconds = Units.Milliseconds.Microseconds(milliseconds);
             var ts = TimeSpan.FromSeconds(seconds);
             var amp = Mathf.Clamp(Mathf.RoundToInt(amplitude * 5), 0, 5);
             if (amp > 0)
             {
                 Controller?.TriggerHapticPulse(
-                    (ushort)nanoseconds,
+                    (ushort)microseconds,
                     WVR_InputId.WVR_InputId_Alias1_Trigger);
             }
             while((DateTime.Now - start) < ts)
