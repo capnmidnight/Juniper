e28c71a577fa893c86e020293a2c41e9058fe4f1
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2019-09-16 09:28:44 -0400

    skin colors

--- src/Juniper/Assets/Juniper/Scripts/HSVColor.cs
+++ src/Juniper/Assets/Juniper/Scripts/HSVColor.cs
@@ -8,8 +8,54 @@ namespace Juniper
     /// <summary>
     /// The Hue/Saturation/Value color space.
     /// </summary>
-    public struct HSVColor
+    public struct HSVColor : IEquatable<HSVColor>, IEquatable<Color>
     {
+        private static readonly int[] SkinColors = {
+            0xFFDFC4,
+            0xF0D5BE,
+            0xEECEB3,
+            0xE1B899,
+            0xE5C298,
+            0xFFDCB2,
+            0xE5B887,
+            0xE5A073,
+            0xE79E6D,
+            0xDB9065,
+            0xCE967C,
+            0xC67856,
+            0xBA6C49,
+            0xA57257,
+            0xF0C8C9,
+            0xDDA8A0,
+            0xB97C6D,
+            0xA8756C,
+            0xAD6452,
+            0x5C3836,
+            0xCB8442,
+            0xBD723C,
+            0x704139,
+            0xA3866A,
+            0x870400,
+            0x710101,
+            0x430000,
+            0x5B0001,
+            0x302E2E
+        };
+
+        public static readonly Color[] SkinColorsRGB = SkinColors
+            .Select(c =>
+            {
+                var r = (c & 0xff0000) >> 16;
+                var g = (c & 0x00ff00) >> 8;
+                var b = (c & 0x0000ff);
+                return new Color(r / 255f, g / 255f, b / 255f);
+            })
+            .ToArray();
+
+        public static readonly HSVColor[] SkinColorsHSV = SkinColorsRGB
+            .Select(c => (HSVColor)c)
+            .ToArray();
+
         /// <summary>
         /// The Hue
         /// </summary>
