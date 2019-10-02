73884c46b6e53d8f7f75f4c178195c8a82087057
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2019-10-02 15:42:28 -0400

    a useful function

---
+++ src/Juniper/Assets/Juniper/Editor/EditorExt.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UnityEditor
+{
+    public static class EditorExt
+    {
+        public static void ShowScriptField(this Editor editor, MonoBehaviour value)
+        {
+            var script = MonoScript.FromMonoBehaviour(value);
+            using (new EditorGUI.DisabledScope(true))
+            {
+                EditorGUILayout.ObjectField("Script", script, typeof(MonoScript), false);
+            }
+        }
+    }
+}
