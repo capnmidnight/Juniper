ddb163d053095591865c1b4c63813c254e78fd26
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2019-10-02 16:50:10 -0400

    split files

---
+++ src/Juniper.Azure/CognitiveServices/VoicesClient.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading.Tasks;
+using Juniper.HTTP;
+using Juniper.IO;
+
+namespace Juniper.Azure.CognitiveServices
+{
+    public class VoicesClient
+    {
+        private readonly string azureSubscriptionKey;
+        private readonly ITextDecoder<Voice[]> voiceListDecoder;
+
+        private string authToken;
+        private Voice[] voices;
+
+        protected readonly string azureRegion;
+        protected readonly CachingStrategy cache;
+
+        public VoicesClient(string azureRegion, string azureSubscriptionKey, ITextDecoder<Voice[]> voiceListDecoder, CachingStrategy cache)
+        {
+            if (string.IsNullOrEmpty(azureRegion))
+            {
+                throw new ArgumentException("Must provide an service region", nameof(azureRegion));
+            }
+
+            if (string.IsNullOrEmpty(azureSubscriptionKey))
+            {
+                throw new ArgumentException("Must provide a subscription key", nameof(azureSubscriptionKey));
+            }
+
+            if (voiceListDecoder == null
+                || voiceListDecoder.ContentType != MediaType.Application.Json)
+            {
+                throw new ArgumentException("Most provide a JSON deserializer for the voice list data", nameof(voiceListDecoder));
+            }
+
+            this.azureRegion = azureRegion;
+            this.azureSubscriptionKey = azureSubscriptionKey;
+            this.voiceListDecoder = voiceListDecoder;
+            this.cache = cache;
+        }
+
+        public VoicesClient(string azureRegion, string azureSubscriptionKey, ITextDecoder<Voice[]> voiceListDecoder)
+            : this(azureRegion, azureSubscriptionKey, voiceListDecoder, null)
+        { }
+
+        public bool IsAvailable { get; protected set; } = true;
+
+        public void ClearError()
+        {
+            IsAvailable = true;
+        }
+
+        protected async Task<string> GetAuthToken()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(authToken))
+                {
+                    var plainText = new StringFactory();
+                    var authRequest = new AuthTokenRequest(azureRegion, azureSubscriptionKey);
+                    authToken = await authRequest.Decode(plainText);
+                }
+                return authToken;
+            }
+            catch
+            {
+                IsAvailable = false;
+                throw;
+            }
+        }
+
+        public async Task<Voice[]> GetVoices()
+        {
+            try
+            {
+                if (voices is null)
+                {
+                    var voiceListRequest = new VoiceListRequest(azureRegion);
+                    if (!cache.IsCached(voiceListRequest))
+                    {
+                        voiceListRequest.AuthToken = await GetAuthToken();
+                    }
+                    voices = await cache.Decode(voiceListRequest, voiceListDecoder);
+                }
+
+                return voices;
+            }
+            catch
+            {
+                IsAvailable = false;
+                throw;
+            }
+        }
+    }
+}
