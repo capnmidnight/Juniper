79b771cd7128e7cce1a223f29e49a4c43e0f6c7e
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2019-11-25 15:35:49 -0500

    lock menus to screen on desktop

--- src/Juniper/Assets/Juniper/Scripts/Widgets/FollowMainCamera.cs
+++ src/Juniper/Assets/Juniper/Scripts/Widgets/FollowMainCamera.cs
@@ -23,13 +23,11 @@ namespace Juniper.Widgets
             // TODO: figure out what is best for different systems. This is just a placeholder for
             // now that works well in the Editor and on Daydream, but may not be suitable for other modalities.
 
-#if UNITY_XR_ARCORE || UNITY_XR_ARKIT || STANDARD_DISPLAY && (ANDROID || IOS)
+#if STANDARD_DISPLAY
             f.interpolate = false;
             f.FollowThreshold = 0f;
             f.FollowRotation = CartesianAxisFlags.XY;
             f.RotationThreshold = Vector3.zero;
-            f.maxSpeed = 1000;
-            f.maxRotationRate = 1000;
 #else
             f.interpolate = true;
             f.FollowThreshold = 0.5f;
