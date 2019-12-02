700982f97aff01ae367388a35bb24371a07f79d7
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2019-12-02 00:47:35 -0500

    a useful class

---
+++ src/Juniper.Root/HTTP/GetRequest.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juniper.HTTP
+{
+    public class GetRequest : REST.AbstractRequest<MediaType>
+    {
+        public GetRequest(Uri url, MediaType contentType)
+            : base(HttpMethod.GET, url, contentType)
+        { }
+
+        public override string CacheID
+        {
+            get
+            {
+                var parts = BaseURI
+                    .PathAndQuery
+                    .SplitX('/');
+
+                if (parts.Length > 0)
+                {
+                    var last = parts.Length - 1;
+                    foreach (var c in Path.GetInvalidFileNameChars())
+                    {
+                        parts[last] = parts[last].Replace(c, '_');
+                    }
+                }
+
+                return Path.Combine(parts
+                    .Prepend(BaseURI.Host)
+                    .ToArray());
+            }
+        }
+    }
+}
