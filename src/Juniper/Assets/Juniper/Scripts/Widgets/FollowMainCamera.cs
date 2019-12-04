d3d917481c357e8c07e2735434456ee455f51fed
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2019-12-04 11:50:54 -0500

    make FollowMainCamera respect existing FollowObject settings

--- src/Juniper/Assets/Juniper/Scripts/Widgets/FollowMainCamera.cs
+++ src/Juniper/Assets/Juniper/Scripts/Widgets/FollowMainCamera.cs
@@ -10,33 +10,32 @@ namespace Juniper.Widgets
         public float followDistance;
         private FollowObject f;
 
-        public void Awake()
-        {
-            f = this.Ensure<FollowObject>();
-
-            f.followObject = DisplayManager.MainCamera.transform;
-            f.FollowPosition = CartesianAxisFlags.XYZ;
-        }
-
         public void Start()
         {
             // TODO: figure out what is best for different systems. This is just a placeholder for
             // now that works well in the Editor and on Daydream, but may not be suitable for other modalities.
+            var f = this.Ensure<FollowObject>();
 
+            f.Value.followObject = DisplayManager.MainCamera.transform;
+            f.Value.FollowPosition = CartesianAxisFlags.XYZ;
+            f.Value.Distance = followDistance;
+
+            if(f.IsNew)
+            {
 #if STANDARD_DISPLAY
-            f.interpolate = false;
-            f.FollowThreshold = 0f;
-            f.FollowRotation = CartesianAxisFlags.XY;
-            f.RotationThreshold = Vector3.zero;
+                f.Value.interpolate = false;
+                f.Value.FollowThreshold = 0f;
+                f.Value.FollowRotation = CartesianAxisFlags.XY;
+                f.Value.RotationThreshold = Vector3.zero;
 #else
-            f.interpolate = true;
-            f.FollowThreshold = 0.5f;
-            f.FollowRotation = CartesianAxisFlags.Y;
-            f.RotationThreshold = 25 * Vector3.up;
-            f.maxSpeed = 5;
-            f.maxRotationRate = 250;
+                f.Value.interpolate = true;
+                f.Value.FollowThreshold = 0.5f;
+                f.Value.FollowRotation = CartesianAxisFlags.Y;
+                f.Value.RotationThreshold = 25 * Vector3.up;
+                f.Value.maxSpeed = 5;
+                f.Value.maxRotationRate = 250;
 #endif
-            f.Distance = followDistance;
+            }
         }
     }
 }
