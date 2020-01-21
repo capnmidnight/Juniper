def34e5aec83cb5adf70a6435ae7532d4f5586f4
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2020-01-20 23:39:49 -0500

    moving files around

---
+++ src/Veldrid - Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Veldrid_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
