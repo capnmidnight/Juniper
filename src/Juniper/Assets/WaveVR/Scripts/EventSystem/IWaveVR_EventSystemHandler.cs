3d247baa40d0e3efc8442664e16db3dcb76bbc9d
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2020-02-12 17:43:06 -0500

    moving files

--- src/Juniper/Assets/WaveVR/Scripts/EventSystem/IWaveVR_EventSystemHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine.EventSystems;
-
-/// Handler for pointer is hovering over GameObject.
-public interface IPointerHoverHandler : IEventSystemHandler
-{
-    void OnPointerHover (PointerEventData eventData);
-}
