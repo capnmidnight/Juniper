89f3c2270b46aefafc59f5976e71d31702aafd79
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2020-02-26 00:24:40 -0500

    accessing OpenVR directly (might use this one day)

---
+++ src/OpenVR - Console/Program.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Juniper.OpenVR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Is Runtime Installed: {0}", Valve.VR.OpenVR.IsRuntimeInstalled());
+            var isHmdPresent = Valve.VR.OpenVR.IsHmdPresent();
+            Console.WriteLine("Is HMD Present: {0}", isHmdPresent);
+            if (isHmdPresent)
+            {
+                try
+                {
+                    var err = Valve.VR.EVRInitError.None;
+                    var sys = Valve.VR.OpenVR.Init(ref err, Valve.VR.EVRApplicationType.VRApplication_Scene);
+                    if (err != Valve.VR.EVRInitError.None)
+                    {
+                        Console.WriteLine("Err {0}", err);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Runtime version: {0}", sys.GetRuntimeVersion());
+                        Console.WriteLine("Is display on desktop: {0}", sys.IsDisplayOnDesktop());
+                        Console.WriteLine("Is input available: {0}", sys.IsInputAvailable());
+                        Console.WriteLine("Is SteamVR drawing controllers: {0}", sys.IsSteamVRDrawingControllers());
+                        Console.WriteLine("Should application pause: {0}", sys.ShouldApplicationPause());
+                        Console.WriteLine("Should application reduce rendering work: {0}", sys.ShouldApplicationReduceRenderingWork());
+                        sys.GetRecommendedRenderTargetSize(out var width, out var height);
+                        Console.WriteLine("Recommended render target size: {0}x{1}", width, height);
+                        Console.WriteLine("Button Name: {0}", sys.GetButtonIdNameFromEnum(Valve.VR.EVRButtonId.k_EButton_A));
+                    }
+                }
+                finally
+                {
+                    Valve.VR.OpenVR.Shutdown();
+                }
+            }
+        }
+    }
+}
