d1cc9315a99b7a3f8036aed6ed5f783a239acafc
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2020-03-04 15:02:34 -0500

    rename Pose -> PoseSerializable

--- src/Juniper.Root/XR/PoseSerializable.cs
+++ src/Juniper.Root/XR/PoseSerializable.cs
@@ -6,25 +6,25 @@ using Juniper.Mathematics;
 namespace Juniper.XR
 {
     [Serializable]
-    public struct Pose : ISerializable, IEquatable<Pose>
+    public struct PoseSerializable : ISerializable, IEquatable<PoseSerializable>
     {
-        public static readonly Pose Identity = new Pose(0, 0, 0, 0, 0, 0, 1);
+        public static readonly PoseSerializable Identity = new PoseSerializable(0, 0, 0, 0, 0, 0, 1);
 
         public Vector3Serializable Position { get; }
         public QuaternionSerializable Orientation { get; }
 
-        public Pose(Vector3Serializable position, QuaternionSerializable orientation)
+        public PoseSerializable(Vector3Serializable position, QuaternionSerializable orientation)
         {
             Position = position;
             Orientation = orientation;
         }
 
-        public Pose(float px, float py, float pz, float ox, float oy, float oz, float ow)
+        public PoseSerializable(float px, float py, float pz, float ox, float oy, float oz, float ow)
             : this(new Vector3Serializable(px, py, pz), new QuaternionSerializable(ox, oy, oz, ow))
         { }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Parameter `context` is required by ISerializable interface")]
-        private Pose(SerializationInfo info, StreamingContext context)
+        private PoseSerializable(SerializationInfo info, StreamingContext context)
             : this(info?.GetVector3(nameof(Position)) ?? throw new ArgumentNullException(nameof(info)),
                 info.GetQuaternion(nameof(Orientation)))
         { }
@@ -42,11 +42,11 @@ namespace Juniper.XR
 
         public override bool Equals(object obj)
         {
-            return obj is Pose other
+            return obj is PoseSerializable other
                 && Equals(other);
         }
 
-        public bool Equals(Pose other)
+        public bool Equals(PoseSerializable other)
         {
             return Position.Equals(other.Position)
                 && Orientation.Equals(other.Orientation);
@@ -58,12 +58,12 @@ namespace Juniper.XR
                 ^ Orientation.GetHashCode();
         }
 
-        public static bool operator ==(Pose left, Pose right)
+        public static bool operator ==(PoseSerializable left, PoseSerializable right)
         {
             return left.Equals(right);
         }
 
-        public static bool operator !=(Pose left, Pose right)
+        public static bool operator !=(PoseSerializable left, PoseSerializable right)
         {
             return !(left == right);
         }
