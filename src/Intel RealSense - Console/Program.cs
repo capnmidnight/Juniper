57122afa06295b28ac786b32a3151f58a5bfb365
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2020-03-05 13:15:36 -0500

    more conversion of RealSense sample

--- src/Intel RealSense - Console/Program.cs
+++ src/Intel RealSense - Console/Program.cs
@@ -1,13 +1,31 @@
+using System;
+using System.Numerics;
+using System.Threading;
+using System.Windows.Forms;
+
 using Intel.RealSense;
+
+using static System.Console;
+
+using Pose = Juniper.Mathematics.Pose;
 
 namespace Juniper
 {
+
     public static class Program
     {
         const int fisheySensorIdx = 1;
 
+        private static Pose objectPoseInWorld, devicePoseInWorld;
+
         public static void Main()
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += Application_ThreadException;
+
+            WriteLine("Waiting for device...");
+
             using var pipe = new Pipeline();
             using var cfg = new Config();
 
@@ -24,6 +42,41 @@ namespace Juniper
                 .GetIntrinsics();
             var poseToFisheyeExtrinsics = poseStream.GetExtrinsicsTo(fisheyeStream);
 
+            WriteLine("Device found. Streaming data...");
+
+            using var form = new MainForm();
+            form.ClientSize = new System.Drawing.Size(intrinsics.width, intrinsics.height);
+            form.KeyUp += Form_KeyUp;
+
+            using var graphics = form.CreateGraphics();
+
+            Application.Run(form);
+        }
+
+        private static void Form_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Space:
+                objectPoseInWorld = ResetObjectPose(devicePoseInWorld);
+                break;
+            }
+        }
+
+        private static Pose DEFAULT_POSE = new Pose
+        {
+            Position = new Vector3(0, 0, -0.5f),
+            Orientation = Quaternion.Identity
+        };
+
+        private static Pose ResetObjectPose(Pose devicePoseInWorld)
+        {
+            return devicePoseInWorld * DEFAULT_POSE;
+        }
+
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            Error.WriteLine(e.Exception.Unroll());
         }
     }
 }
