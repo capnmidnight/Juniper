06a642c50c47c7000055205cdfd538fd728db322
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2020-03-07 00:47:43 -0500

    sink mesh functions int Mesh type

--- src/Juniper.Veldrid/Mesh.cs
+++ src/Juniper.Veldrid/Mesh.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
+
 using Veldrid;
+
 using IndexT = System.UInt16;
 
 namespace Juniper.VeldridIntegration
@@ -8,13 +12,52 @@ namespace Juniper.VeldridIntegration
         public static Mesh<VertexT> OfQuads<VertexT>(VertexT[] vertices, IndexT[] indices)
             where VertexT : struct
         {
-            return new Mesh<VertexT>(vertices.ToQuads(indices), vertices, indices);
+            if (vertices is null)
+            {
+                throw new ArgumentNullException(nameof(vertices));
+            }
+
+            if (indices is null)
+            {
+                throw new ArgumentNullException(nameof(indices));
+            }
+
+            var quads = new List<Quad<VertexT>>();
+            for (var i = 0; i < indices.Length; i += 4)
+            {
+                quads.Add(new Quad<VertexT>(
+                    vertices[indices[i]],
+                    vertices[indices[i + 1]],
+                    vertices[indices[i + 2]],
+                    vertices[indices[i + 3]]));
+            }
+
+            return new Mesh<VertexT>(quads.ToArray(), vertices, indices);
         }
 
         public static Mesh<VertexT> OfTris<VertexT>(VertexT[] vertices, IndexT[] indices)
             where VertexT : struct
         {
-            return new Mesh<VertexT>(vertices.ToTriangles(indices), vertices, indices);
+            if (vertices is null)
+            {
+                throw new ArgumentNullException(nameof(vertices));
+            }
+
+            if (indices is null)
+            {
+                throw new ArgumentNullException(nameof(indices));
+            }
+
+            var triangles = new List<Triangle<VertexT>>();
+            for (var i = 0; i < indices.Length; i += 3)
+            {
+                triangles.Add(new Triangle<VertexT>(
+                    vertices[indices[i]],
+                    vertices[indices[i + 1]],
+                    vertices[indices[i + 2]]));
+            }
+
+            return new Mesh<VertexT>(triangles.ToArray(), vertices, indices);
         }
 
         internal abstract void Draw(CommandList commandList);
