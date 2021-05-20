6962b6c1007d89cd552151e292133adc38468914
Sean T. McBeth <sean.mcbeth@gmail.com>
2021-05-19 22:03:19 -0400

    missed a file format

--- src/Juniper.Root/XShader.Values.cs
+++ src/Juniper.Root/XShader.Values.cs
@@ -8,7 +8,8 @@ namespace Juniper
             public static readonly XShader X_Fragment = new XShader("x-fragment", new string[] { "frag", "glsl" });
 
             public static new readonly XShader[] Values = {
-                X_Vertex
+                X_Vertex,
+                X_Fragment
             };
         }
     }
