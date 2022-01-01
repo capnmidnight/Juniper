3f403f388204d0a0827e95c847eb7e8c987ea2ec
Sean T. McBeth <sean.mcbeth@gmail.com>
2021-12-31 21:59:15 -0500

    rebuild script

---
+++ rebuild.ps1
@@ -0,0 +1,17 @@
+cd src\Juniper.TypeScript
+cd .\esbuild; npm i; npm run build
+cd ..\emoji; npm i; npm run build
+cd ..\tslib; npm i; npm run build
+cd ..\mediatypes; npm i; npm run build
+cd ..\dom; npm i; npm run build
+cd ..\fetcher-base; npm i; npm run build
+cd ..\fetcher; npm i; npm run build
+cd ..\fetcher-worker; npm i; npm run build
+cd ..\google-maps; npm i; npm run build
+cd ..\graphics2d; npm i; npm run build
+cd ..\testing; npm i; npm run build
+cd ..\units; npm i; npm run build
+cd ..\audio; npm i; npm run build
+cd ..\webrtc; npm i; npm run build
+cd ..\threejs; npm i; npm run build
+cd ..\..\
