74c6f99b7fdb42e61de0b4b4c40cc073a9716689
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-01-11 16:15:05 -0500

    new playback button

--- src/Juniper.TypeScript/threejs/PlaybackButton.ts
+++ src/Juniper.TypeScript/threejs/PlaybackButton.ts
@@ -1,104 +1,134 @@
 import type { IPlayableSource } from "juniper-audio/sources/IPlayableSource";
 import { MouseButtons } from "juniper-dom/eventSystem/MouseButton";
-import {
-    pauseButton,
-    playButton,
-    speakerHighVolume,
-    speakerLowVolume
-} from "juniper-emoji";
+import { keycapDigits } from "juniper-emoji/numbers";
 import { TypedEvent, TypedEventBase } from "juniper-tslib";
+import { ButtonFactory } from "./ButtonFactory";
 import { EventSystemThreeJSEvent } from "./eventSystem/EventSystemEvent";
-import { ErsatzObject, obj } from "./objects";
-import { TextMeshButton } from "./TextMeshButton";
+import { MeshButton } from "./MeshButton";
+import { ErsatzObject, obj, objGraph } from "./objects";
+import { TextMeshLabel } from "./TextMeshLabel";
 
-
-function makePauseButtonLabel(label: string): string {
-    return label + pauseButton.value + speakerHighVolume.value;
-}
-
-function makePlayButtonLabel(label: string): string {
-    return label + playButton.value + speakerLowVolume.value;
-}
 
 const playEvt = new TypedEvent("play");
 const stopEvt = new TypedEvent("stop");
+const size = 0.1;
 interface PlaybackButtonEvents {
     play: TypedEvent<"play">;
     stop: TypedEvent<"stop">;
 }
+
+const translations = new Map(
+    keycapDigits.alts.map((m, i) => [m.value, i.toString()])
+);
 
 export class PlaybackButton
     extends TypedEventBase<PlaybackButtonEvents>
     implements ErsatzObject {
 
     readonly object: THREE.Object3D;
-    playButton: TextMeshButton;
-    pauseButton: TextMeshButton;
 
-    private _label: string;
+    private readonly textLabel: TextMeshLabel;
+    private playButton: MeshButton = null;
+    private pauseButton: MeshButton = null;
 
     constructor(
+        buttonFactory: ButtonFactory,
         name: string,
         label: string,
         clip: IPlayableSource) {
         super();
 
-        this.object = obj(
-            `play-${name}`,
+        label = translations.get(label) || label;
 
-            this.playButton = new TextMeshButton(
-                `playbackButtonPlay${label}`,
-                makePlayButtonLabel(label)
-            ),
+        this.object = obj(`playback-${name}`);
 
-            this.pauseButton = new TextMeshButton(
-                `playbackButtonPause${label}`,
-                makePauseButtonLabel(label)
+        this.textLabel = new TextMeshLabel(`playback-${name}-label`, label, {
+            minHeight: size,
+            maxHeight: size,
+            minWidth: size,
+            padding: 0.02,
+            scale: 1000,
+            bgFillColor: "#1e4388"
+        });
+
+        this.load(buttonFactory, clip);
+    }
+
+    private async load(buttonFactory: ButtonFactory, clip: IPlayableSource) {
+        const [
+            enabledMaterial,
+            disabledMaterial,
+            playGeometry,
+            pauseGeometry
+        ] = await Promise.all([
+            buttonFactory.getMaterial(true),
+            buttonFactory.getMaterial(false),
+            buttonFactory.getGeometry("media", "play"),
+            buttonFactory.getGeometry("media", "pause")
+        ]);
+
+        objGraph(
+            this.object,
+            this.textLabel,
+            this.playButton = new MeshButton(
+                "PlayButton",
+                playGeometry,
+                enabledMaterial,
+                disabledMaterial,
+                size),
+            this.pauseButton = new MeshButton(
+                "PauseButton",
+                pauseGeometry,
+                enabledMaterial,
+                disabledMaterial,
+                size
             )
         );
 
-        let isPlaying = false;
+        this.object.children.forEach((child, i, arr) =>
+            child.position.x = (i - arr.length / 2) * size);
+
+        this.isPlaying = false;
 
         const onStop = (evt: EventSystemThreeJSEvent<"click">) => {
-            if ((!evt || evt.buttons === MouseButtons.Mouse0) && isPlaying) {
-                this.playButton.visible = true;
-                this.pauseButton.visible = false;
+            if ((!evt || evt.buttons === MouseButtons.Mouse0) && this.isPlaying) {
                 clip.stop();
                 this.dispatchEvent(stopEvt);
-                isPlaying = false;
+                this.isPlaying = false;
             }
         };
-
-        this._label = label;
 
         this.playButton.addEventListener("click", async (ev: THREE.Event) => {
             const evt = ev as EventSystemThreeJSEvent<"click">;
             if (evt.buttons === MouseButtons.Mouse0) {
-                this.playButton.visible = false;
-                this.pauseButton.visible = true;
+                this.isPlaying = true;
                 this.dispatchEvent(playEvt);
-                isPlaying = true;
                 await clip.play();
                 onStop(evt);
             }
         });
 
-        this.pauseButton.visible = false;
         this.pauseButton.addEventListener("click", (ev: THREE.Event) => {
             const evt = ev as EventSystemThreeJSEvent<"click">;
             onStop(evt);
         });
     }
 
+    private get isPlaying() {
+        return !this.pauseButton.disabled;
+    }
+
+    private set isPlaying(v) {
+        this.playButton.disabled = v;
+        this.pauseButton.disabled = !v;
+    }
+
     get label(): string {
-        return this._label;
+        return this.textLabel.image.value;
     }
 
     set label(v: string) {
-        if (v !== this.label) {
-            this._label = v;
-            this.playButton.image.value = makePlayButtonLabel(v);
-            this.pauseButton.image.value = makePauseButtonLabel(v);
-        }
+        v = translations.get(v) || v;
+        this.textLabel.image.value = v;
     }
 }
