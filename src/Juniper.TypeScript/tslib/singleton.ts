557bcc0cce25204ac0f289183ce38458ebf757e9
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-01-12 15:44:39 -0500

    option to retrieve singleton without creating it

--- src/Juniper.TypeScript/tslib/singleton.ts
+++ src/Juniper.TypeScript/tslib/singleton.ts
@@ -1,9 +1,12 @@
 import { isNullOrUndefined } from "./typeChecks";
 
-export function singleton<T>(name: string, create: () => T): T {
+export function singleton<T>(name: string, create?: () => T): T {
     const box = globalThis as any;
     let value = box[name] as T;
     if (isNullOrUndefined(value)) {
+        if (isNullOrUndefined(create)) {
+            throw new Error(`No value ${name} found`);
+        }
         value = create();
         box[name] = value;
     }
