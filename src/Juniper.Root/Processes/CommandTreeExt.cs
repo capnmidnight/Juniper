b6c9e1e7ba86c43f003d1b05492d8befd43f96ed
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-01-20 01:36:53 -0500

    standard Juniper TypeScript commands

---
+++ src/Juniper.Root/Processes/CommandTreeExt.cs
@@ -0,0 +1,65 @@
+using System.IO;
+using System.Linq;
+
+namespace Juniper.Processes
+{
+    public static class CommandTreeExt
+    {
+        private static ShellCommand NPM(DirectoryInfo juniperDir, string name, string cmd)
+        {
+            return new ShellCommand("npm", "run", "--prefix", Path.Combine(juniperDir.FullName, "src", "Juniper.TypeScript", name), cmd);
+        }
+
+        private static readonly string[] names = new[]
+            {
+                "esbuild",
+                "emoji",
+                "tslib",
+                "mediatypes",
+                "dom",
+                "fetcher-base",
+                "fetcher",
+                "fetcher-worker",
+                "google-maps",
+                "graphics2d",
+                "testing",
+                "units",
+                "audio",
+                "webrtc",
+                "threejs"
+            };
+
+        public static ICommandTree InitJuniper(this ICommandTree commands, DirectoryInfo juniperDir)
+        {
+            return commands.AddCommands(names.Select(name => NPM(juniperDir, name, "init")));
+        }
+
+        public static ICommandTree BuildJuniper(this ICommandTree commands, DirectoryInfo juniperDir)
+        {
+            return commands.AddCommands(names.Select(name => NPM(juniperDir, name, "build")));
+        }
+
+        static CopyCommand Copy(DirectoryInfo juniperDir, DirectoryInfo outputDir, string from, string to)
+        {
+            return new CopyCommand(
+                Path.Combine(
+                    juniperDir.FullName,
+                    "src",
+                    "Juniper.TypeScript",
+                    PathExt.FixPath(from)),
+                Path.Combine(
+                    outputDir.FullName,
+                    "wwwroot",
+                    PathExt.FixPath(to)));
+        }
+
+        public static ICommandTree CopyJuniperScripts(this ICommandTree commands, DirectoryInfo juniperDir, DirectoryInfo outputDir)
+        {
+            return commands.AddCommands(
+                Copy(juniperDir, outputDir, "fetcher-worker/dist/index.js", "workers/fetcher/index.js"),
+                Copy(juniperDir, outputDir, "fetcher-worker/dist/index.js.map", "workers/fetcher/index.js.map"),
+                Copy(juniperDir, outputDir, "fetcher-worker/dist/index.min.js", "workers/fetcher/index.min.js"),
+                Copy(juniperDir, outputDir, "fetcher-worker/dist/index.min.js.map", "workers/fetcher/index.min.js.map"));
+        }
+    }
+}
