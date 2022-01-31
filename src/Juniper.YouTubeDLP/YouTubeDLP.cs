414eef1ba8210faa289f16aedb95d6f128c244bd
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-01-30 23:33:59 -0500

    structure output

--- src/Juniper.YouTubeDLP/YouTubeDLP.cs
+++ src/Juniper.YouTubeDLP/YouTubeDLP.cs
@@ -10,7 +10,11 @@ namespace Juniper
         public static IEnumerable<string> AttemptPaths => FindCommandPaths(YT_DLP);
 
         private static readonly HttpClient http = new();
-        public record Output(string? ContentType, string Url);
+
+        public record MediaEntry(string? ContentType, string Url);
+
+        public record Result(MediaEntry? Video, MediaEntry? Audio);
+
 
         private YouTubeDLP(string youtubeUrl)
             : base(YT_DLP, "--get-url", youtubeUrl)
@@ -18,19 +22,21 @@ namespace Juniper
 
         }
 
-        public static async Task<Output[]> GetURLs(string youtubeUrl)
+        public static async Task<Result> GetURLs(string youtubeUrl)
         {
             var cmd = new YouTubeDLP(youtubeUrl);
             var urls = await cmd.RunForStdOutAsync();
             var output = await Task.WhenAll(urls.Select(GetURL));
-            return output.ToArray();
+            var vid = output.FirstOrDefault(v => MediaType.Video.AnyVideo.Matches(v.ContentType));
+            var aud = output.FirstOrDefault(v => MediaType.Audio.AnyAudio.Matches(v.ContentType));
+            return new Result(vid, aud);
         }
 
-        private static async Task<Output> GetURL(string url)
+        private static async Task<MediaEntry> GetURL(string url)
         {
             using var request = new HttpRequestMessage(HttpMethod.Head, url);
             using var response = await http.SendAsync(request);
-            return new Output(response?.Content?.Headers?.ContentType?.MediaType, url);
+            return new MediaEntry(response?.Content?.Headers?.ContentType?.MediaType, url);
         }
     }
 }
