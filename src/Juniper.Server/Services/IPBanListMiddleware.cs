ae17dc71fda95168351f4721399b0756beb8f412
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-02-10 15:10:56 -0500

    ban sources by IP address

---
+++ src/Juniper.Server/Services/IPBanListMiddleware.cs
@@ -0,0 +1,39 @@
+using System.Net;
+
+namespace Juniper.Services
+{
+    public static class IPBanListMiddlewareExt
+    {
+        public static IApplicationBuilder UseIPBanList(this IApplicationBuilder app, string bannedIPs)
+        {
+            return app.UseMiddleware<IPBanListMiddleware>(bannedIPs);
+        }
+    }
+
+    public class IPBanListMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly IPAddress[] bannedIPs;
+
+        public IPBanListMiddleware(RequestDelegate next, string bannedIPs)
+        {
+            this.next = next;
+            this.bannedIPs = bannedIPs.SplitX(';')
+                .Select(IPAddress.Parse)
+                .ToArray();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var remoteIp = context.Connection.RemoteIpAddress;
+            if (remoteIp is null
+                || bannedIPs.Any(remoteIp.Equals))
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                return;
+            }
+
+            await next.Invoke(context);
+        }
+    }
+}
