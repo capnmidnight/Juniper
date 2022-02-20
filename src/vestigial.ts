cb6cf533dfb9df40ce6d50ce14ee67fdf4b3452c
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-02-19 19:22:05 -0500

    really keep Visual Studio quiet

---
+++ src/vestigial.ts
@@ -0,0 +1,6 @@
+/**
+ * This file exists just to keep Visual Studio from complaining
+ * about the root tsconfig.json file not having any inputs, or
+ * finding all the other scripts in this directory, but not having
+ * the 3rd party libraries configured correctly.
+ **/
