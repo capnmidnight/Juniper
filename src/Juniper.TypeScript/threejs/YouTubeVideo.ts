82dab5bd0ae9a5370ab629ddf91e598bd7b24d23
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-02-22 20:01:50 -0500

    a full object here is overkill

--- src/Juniper.TypeScript/threejs/YouTubeVideo.ts
+++ src/Juniper.TypeScript/threejs/YouTubeVideo.ts
@@ -6,13 +6,12 @@ import { IProgress, progressSplitWeighted } from "juniper-tslib";
 import { createQuadGeometry } from "./CustomGeometry";
 import { Environment } from "./environment/Environment";
 import { solid } from "./materials";
-import { objGraph } from "./objects";
 import { PlaybackButton } from "./PlaybackButton";
 
 const I = 1 / 3;
 const J = 2 / 3;
 const K = 1 / 2;
-const YouTubeCubeMapGeom = createQuadGeometry([
+const YouTube360MonoCubeMapGeom = createQuadGeometry([
     [-K, +K, -K, I, 1],
     [+K, +K, -K, J, 1],
     [+K, -K, -K, J, K],
@@ -46,57 +45,39 @@ const YouTubeCubeMapGeom = createQuadGeometry([
 
 type YtDlpCallback = (pageUrl: string, fetcher: IFetcher, prog?: IProgress) => Promise<YTBasicResult>;
 
-export class YouTubeVideo extends THREE.Object3D {
+interface VideoPlayerResult {
+    controls: PlaybackButton;
+    videoMesh: THREE.Mesh;
+}
 
-    constructor(private readonly pageUrl: string, private readonly env: Environment, private readonly queryYtDlp: YtDlpCallback) {
-        super();
+export async function loadYouTube360MonoVideo(env: Environment, pageUrl: string, queryYtDlp: YtDlpCallback, prog: IProgress): Promise<VideoPlayerResult> {
+    const progs = progressSplitWeighted(prog, [1, 10, 100]);
+    const { video, audio } = await queryYtDlp(pageUrl, env.fetcher, progs.pop());
+
+    console.log("Getting video and audio");
+
+    const [videoClip, audioClip] = await Promise.all([
+        mediaElementReady(BackgroundVideo(false, true, false, src(video.url))),
+        env.audio.createBasicClip("audio", audio.url, 1, progs.pop())
+    ]);
+
+    console.log("Got video and audio", videoClip, audioClip);
+
+    if (audioClip instanceof AudioElementSource) {
+        mediaElementForwardEvents(audioClip.input.mediaElement, videoClip);
     }
 
-    private _pb: PlaybackButton = null;
-    get playbackButton(): THREE.Object3D {
-        if (this._pb === null) {
-            return null;
-        }
+    const controls = new PlaybackButton(env, env.uiButtons, "video", null, audioClip);
+    controls.object.position.set(0, 1, -0.95);
 
-        return this._pb.object;
-    }
+    const videoTexture = new THREE.VideoTexture(videoClip);
 
-    private _vid: THREE.Mesh = null;
-    get video(): THREE.Mesh {
-        return this._vid;
-    }
+    const videoMesh = new THREE.Mesh(YouTube360MonoCubeMapGeom, solid({
+        name: video.url,
+        map: videoTexture,
+        depthWrite: false
+    }));
+    videoMesh.scale.set(100, 100, 100);
 
-    async load(prog: IProgress): Promise<void> {
-        const progs = progressSplitWeighted(prog, [1, 10, 100]);
-        const { video, audio } = await this.queryYtDlp(this.pageUrl, this.env.fetcher, progs.pop());
-
-        console.log("Getting video and audio");
-
-        const [videoClip, audioClip] = await Promise.all([
-            mediaElementReady(BackgroundVideo(false, true, false, src(video.url))),
-            this.env.audio.createBasicClip("audio", audio.url, 1, progs.pop())
-        ]);
-
-        console.log("Got video and audio", videoClip, audioClip);
-
-        if (audioClip instanceof AudioElementSource) {
-            mediaElementForwardEvents(audioClip.input.mediaElement, videoClip);
-        }
-
-        this._pb = new PlaybackButton(this.env, this.env.uiButtons, "video", null, audioClip);
-        this._pb.object.position.set(0, 1, -0.95);
-
-        const vidTex = new THREE.VideoTexture(videoClip);
-
-        this._vid = new THREE.Mesh(YouTubeCubeMapGeom, solid({
-            name: video.url,
-            map: vidTex,
-            depthWrite: false
-        }));
-        this._vid.scale.set(100, 100, 100);
-
-        objGraph(this,
-            this._pb,
-            this._vid);
-    }
+    return { controls, videoMesh };
 }
