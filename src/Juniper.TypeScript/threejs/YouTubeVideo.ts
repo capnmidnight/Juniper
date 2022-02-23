35e14e36c22d7b0fb155c4a9d4bab7abd984ba03
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-02-22 20:48:27 -0500

    stereo 360 video

--- src/Juniper.TypeScript/threejs/YouTubeVideo.ts
+++ src/Juniper.TypeScript/threejs/YouTubeVideo.ts
@@ -6,49 +6,53 @@ import { IProgress, progressSplitWeighted } from "juniper-tslib";
 import { createQuadGeometry } from "./CustomGeometry";
 import { Environment } from "./environment/Environment";
 import { solid } from "./materials";
+import { objGraph } from "./objects";
 import { PlaybackButton } from "./PlaybackButton";
-
-const I = 1 / 3;
-const J = 2 / 3;
-const K = 1 / 2;
-const YouTube360MonoCubeMapGeom = createQuadGeometry([
-    [-K, +K, -K, I, 1],
-    [+K, +K, -K, J, 1],
-    [+K, -K, -K, J, K],
-    [-K, -K, -K, I, K]
-], [
-    [+K, +K, -K, J, 1],
-    [+K, +K, +K, 1, 1],
-    [+K, -K, +K, 1, K],
-    [+K, -K, -K, J, K]
-], [
-    [-K, +K, +K, 0, 1],
-    [-K, +K, -K, I, 1],
-    [-K, -K, -K, I, K],
-    [-K, -K, +K, 0, K]
-], [
-    [+K, +K, +K, J, K],
-    [-K, +K, +K, J, 0],
-    [-K, -K, +K, I, 0],
-    [+K, -K, +K, I, K]
-], [
-    [+K, +K, -K, 1, K],
-    [-K, +K, -K, 1, 0],
-    [-K, +K, +K, J, 0],
-    [+K, +K, +K, J, K]
-], [
-    [+K, -K, +K, I, K],
-    [-K, -K, +K, I, 0],
-    [-K, -K, -K, 0, 0],
-    [+K, -K, -K, 0, K]
-]);
 
 type YtDlpCallback = (pageUrl: string, fetcher: IFetcher, prog?: IProgress) => Promise<YTBasicResult>;
 
 interface VideoPlayerResult {
     controls: PlaybackButton;
-    videoMesh: THREE.Mesh;
+    video: THREE.Object3D;
 }
+
+const _13 = 1 / 3;
+const _14 = 1 / 4;
+const _12 = 1 / 2;
+const _23 = 2 / 3;
+const _34 = 3 / 4;
+
+const YouTube360MonoCubeMapGeom = createQuadGeometry([
+    [-_12, +_12, -_12, _13, 1],
+    [+_12, +_12, -_12, _23, 1],
+    [+_12, -_12, -_12, _23, _12],
+    [-_12, -_12, -_12, _13, _12]
+], [
+    [+_12, +_12, -_12, _23, 1],
+    [+_12, +_12, +_12, 1, 1],
+    [+_12, -_12, +_12, 1, _12],
+    [+_12, -_12, -_12, _23, _12]
+], [
+    [-_12, +_12, +_12, 0, 1],
+    [-_12, +_12, -_12, _13, 1],
+    [-_12, -_12, -_12, _13, _12],
+    [-_12, -_12, +_12, 0, _12]
+], [
+    [+_12, +_12, +_12, _23, _12],
+    [-_12, +_12, +_12, _23, 0],
+    [-_12, -_12, +_12, _13, 0],
+    [+_12, -_12, +_12, _13, _12]
+], [
+    [+_12, +_12, -_12, 1, _12],
+    [-_12, +_12, -_12, 1, 0],
+    [-_12, +_12, +_12, _23, 0],
+    [+_12, +_12, +_12, _23, _12]
+], [
+    [+_12, -_12, +_12, _13, _12],
+    [-_12, -_12, +_12, _13, 0],
+    [-_12, -_12, -_12, 0, 0],
+    [+_12, -_12, -_12, 0, _12]
+]);
 
 export async function loadYouTube360MonoVideo(env: Environment, pageUrl: string, queryYtDlp: YtDlpCallback, prog: IProgress): Promise<VideoPlayerResult> {
     const progs = progressSplitWeighted(prog, [1, 10, 100]);
@@ -79,5 +83,115 @@ export async function loadYouTube360MonoVideo(env: Environment, pageUrl: string,
     }));
     videoMesh.scale.set(100, 100, 100);
 
-    return { controls, videoMesh };
+    return { controls, video: videoMesh };
+}
+
+
+
+const YouTube360StereoLeftCubeMapGeom = createQuadGeometry([
+    [-_12, +_12, -_12, 0, _13],
+    [+_12, +_12, -_12, 0, _23],
+    [+_12, -_12, -_12, _14, _23],
+    [-_12, -_12, -_12, _14, _13]
+], [
+    [+_12, +_12, -_12, 0, _23],
+    [+_12, +_12, +_12, 0, 1],
+    [+_12, -_12, +_12, _14, 1],
+    [+_12, -_12, -_12, _14, _23]
+], [
+    [-_12, +_12, +_12, 0, 0],
+    [-_12, +_12, -_12, 0, _13],
+    [-_12, -_12, -_12, _14, _13],
+    [-_12, -_12, +_12, _14, 0]
+], [
+    [+_12, +_12, +_12, _14, _23],
+    [-_12, +_12, +_12, _12, _23],
+    [-_12, -_12, +_12, _12, _13],
+    [+_12, -_12, +_12, _14, _13]
+], [
+    [+_12, +_12, -_12, _14, 1],
+    [-_12, +_12, -_12, _12, 1],
+    [-_12, +_12, +_12, _12, _23],
+    [+_12, +_12, +_12, _14, _23]
+], [
+    [+_12, -_12, +_12, _14, _13],
+    [-_12, -_12, +_12, _12, _13],
+    [-_12, -_12, -_12, _12, 0],
+    [+_12, -_12, -_12, _14, 0]
+]
+);
+
+const YouTube360StereoRightCubeMapGeom = createQuadGeometry([
+    [-_12, +_12, -_12, _12, _13],
+    [+_12, +_12, -_12, _12, _23],
+    [+_12, -_12, -_12, _34, _23],
+    [-_12, -_12, -_12, _34, _13]
+], [
+    [+_12, +_12, -_12, _12, _23],
+    [+_12, +_12, +_12, _12, 1],
+    [+_12, -_12, +_12, _34, 1],
+    [+_12, -_12, -_12, _34, _23]
+], [
+    [-_12, +_12, +_12, _12, 0],
+    [-_12, +_12, -_12, _12, _13],
+    [-_12, -_12, -_12, _34, _13],
+    [-_12, -_12, +_12, _34, 0]
+], [
+    [+_12, +_12, +_12, _34, _23],
+    [-_12, +_12, +_12, 1, _23],
+    [-_12, -_12, +_12, 1, _13],
+    [+_12, -_12, +_12, _34, _13]
+], [
+    [+_12, +_12, -_12, _34, 1],
+    [-_12, +_12, -_12, 1, 1],
+    [-_12, +_12, +_12, 1, _23],
+    [+_12, +_12, +_12, _34, _23]
+], [
+    [+_12, -_12, +_12, _34, _13],
+    [-_12, -_12, +_12, 1, _13],
+    [-_12, -_12, -_12, 1, 0],
+    [+_12, -_12, -_12, _34, 0]
+]);
+
+export async function loadYouTube360StereoVideo(env: Environment, pageUrl: string, queryYtDlp: YtDlpCallback, prog: IProgress): Promise<VideoPlayerResult> {
+    const progs = progressSplitWeighted(prog, [1, 10, 100]);
+    const { video, audio } = await queryYtDlp(pageUrl, env.fetcher, progs.pop());
+
+    console.log("Getting video and audio", video, audio);
+
+    const [videoClip, audioClip] = await Promise.all([
+        mediaElementReady(BackgroundVideo(false, true, false, src(video.url))),
+        env.audio.createBasicClip("audio", audio.url, 1, progs.pop())
+    ]);
+
+    console.log("Got video and audio", videoClip, audioClip);
+
+    if (audioClip instanceof AudioElementSource) {
+        mediaElementForwardEvents(audioClip.input.mediaElement, videoClip);
+    }
+
+    const controls = new PlaybackButton(env, env.uiButtons, "video", null, audioClip);
+    controls.object.position.set(0, 1, -0.95);
+
+    const videoTexture = new THREE.VideoTexture(videoClip);
+    const videoMaterial = solid({
+        name: video.url,
+        map: videoTexture,
+        depthWrite: false
+    });
+    const videoMeshLeft = new THREE.Mesh(YouTube360StereoLeftCubeMapGeom, videoMaterial);
+    videoMeshLeft.layers.set(2);
+    videoMeshLeft.layers.enable(0);
+    videoMeshLeft.scale.set(100, 100, 100);
+
+    const videoMeshRight = new THREE.Mesh(YouTube360StereoRightCubeMapGeom, videoMaterial);
+    videoMeshRight.layers.set(1);
+    videoMeshRight.scale.set(100, 100, 100);
+
+    const videoObject = new THREE.Object3D();
+    objGraph(videoObject,
+        videoMeshLeft,
+        videoMeshRight
+    );
+    return { controls, video: videoObject };
 }
