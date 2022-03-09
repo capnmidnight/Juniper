db9c9683722d221b9074f522a8953ee6ae15db11
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-03-09 07:58:02 -0500

    convenience response

---
+++ src/Juniper.Server/HTTP/ServerErrorResult.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+using System.Net;
+
+namespace Juniper.HTTP
+{
+    public class ServerErrorResult : StatusCodeResult
+    {
+        public ServerErrorResult() : base((int)HttpStatusCode.InternalServerError)
+        {
+
+        }
+    }
+}
