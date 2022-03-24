a6e0f4889b6d8f3ce843b97afec537cf3822481f
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-03-24 00:26:58 -0400

    boolean functional logic support

--- src/Juniper.TypeScript/tslib/identity.ts
+++ src/Juniper.TypeScript/tslib/identity.ts
@@ -17,8 +17,32 @@ export function alwaysFalse(): false {
     return false;
 }
 
-export function invert(value: boolean) {
+export function not(value: boolean) {
     return !value;
+}
+
+export function and(a: boolean, b: boolean): boolean {
+    return a && b;
+}
+
+export function or(a: boolean, b: boolean): boolean {
+    return a || b;
+}
+
+export function xor(a: boolean, b: boolean): boolean {
+    return a !== b;
+}
+
+export function nand(a: boolean, b: boolean): boolean {
+    return not(and(a, b));
+}
+
+export function nor(a: boolean, b: boolean): boolean {
+    return not(or(a, b));
+}
+
+export function equal<T>(a: T, b: T): boolean {
+    return a === b;
 }
 
 export type AsyncCallback = () => Promise<void>;
