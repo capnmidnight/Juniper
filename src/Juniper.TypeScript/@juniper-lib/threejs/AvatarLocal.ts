864047bea1f0f6d276ff8bbb6ed453f8caa1834f
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-06-23 00:00:34 -0400

    fix screen edge dragging

--- src/Juniper.TypeScript/@juniper-lib/threejs/AvatarLocal.ts
+++ src/Juniper.TypeScript/@juniper-lib/threejs/AvatarLocal.ts
@@ -391,10 +391,9 @@ export class AvatarLocal
             if (this.uv.manhattanLength() > 0) {
                 this.motion
                     .set(
-                        this.scaleRadialComponent(this.uv.x, this.speed.x, this.acceleration.x),
-                        this.scaleRadialComponent(-this.uv.y, this.speed.y, this.acceleration.y))
-                    .multiplyScalar(dt)
-                    .multiply(this.axisControl);
+                        this.scaleRadialComponent(-this.uv.x, this.speed.x, this.acceleration.x),
+                        this.scaleRadialComponent(this.uv.y, this.speed.y, this.acceleration.y))
+                    .multiplyScalar(dt);
 
                 this.setHeading(this.heading + this.motion.x);
                 this.setPitch(this.pitch + this.motion.y, this.minimumX, this.maximumX);
