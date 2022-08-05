801201d1f79543a1e15c69bcadd2f537071a955e
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-08-04 21:15:13 -0400

    cleanup context menu

--- src/Juniper.TypeScript/@juniper-lib/widgets/ContextMenu.ts
+++ src/Juniper.TypeScript/@juniper-lib/widgets/ContextMenu.ts
@@ -1,7 +1,7 @@
 import { className } from "@juniper-lib/dom/attrs";
-import { backgroundColor, border, left, padding, position, rule, styles, top } from "@juniper-lib/dom/css";
+import { backgroundColor, border, display, gridTemplateColumns, left, padding, position, rule, styles, top } from "@juniper-lib/dom/css";
 import { onClick } from "@juniper-lib/dom/evts";
-import { Button, elementApply, elementClearChildren, elementSetDisplay, ErsatzElement, LI, Style, UL } from "@juniper-lib/dom/tags";
+import { Button, Div, elementApply, elementClearChildren, elementSetDisplay, ErsatzElement, Style } from "@juniper-lib/dom/tags";
 import { isDefined, Task } from "@juniper-lib/tslib";
 
 Style(
@@ -9,25 +9,34 @@ Style(
         position("absolute"),
         backgroundColor("white"),
         padding("5px"),
-        border("outset 2px #ccc")
+        border("outset 2px #ccc"),
+        display("grid"),
+        gridTemplateColumns("auto")
     )
 );
 
-export class ContextMenu implements ErsatzElement {
+export class ContextMenu<T> implements ErsatzElement {
 
     element: HTMLElement;
 
     private currentTask: Task<any>;
+    private mouseX = 0;
+    private mouseY = 0;
 
-    constructor() {
-        this.element = UL(
+    constructor(private readonly displayNames: Map<T, string>) {
+        this.element = Div(
             className("context-menu")
         );
 
         elementSetDisplay(this.element, false);
+
+        window.addEventListener("mousemove", (evt) => {
+            this.mouseX = evt.clientX;
+            this.mouseY = evt.clientY;
+        });
     }
 
-    public async show<T extends string>(clientX: number, clientY: number, ...options: T[]): Promise<T | null> {
+    public async show(...options: readonly T[]): Promise<T | null> {
         const hasTask = isDefined(this.currentTask);
         if (hasTask) {
             this.currentTask.resolve("cancel");
@@ -41,19 +50,19 @@ export class ContextMenu implements ErsatzElement {
         elementClearChildren(this.element);
         elementApply(this.element,
             styles(
-                left(`${clientX}px`),
-                top(`${clientY}px`)
+                left(`${this.mouseX}px`),
+                top(`${this.mouseY}px`)
             ),
             ...options.map(option =>
-                LI(Button(
-                    option,
+                Button(
+                    this.displayNames.get(option),
                     onClick(() => this.currentTask.resolve(option), true)
-                ))
+                )
             )
         );
-        elementSetDisplay(this.element, true);
-        clientY = Math.min(clientY, window.innerHeight - this.element.clientHeight - 50);
-        this.element.style.top = `${clientY}px`;
+        elementSetDisplay(this.element, true, "grid");
+        this.mouseY = Math.min(this.mouseY, window.innerHeight - this.element.clientHeight - 50);
+        this.element.style.top = `${this.mouseY}px`;
 
         const onSideClick = () => this.currentTask.resolve("cancel");
         addEventListener("click", onSideClick);
