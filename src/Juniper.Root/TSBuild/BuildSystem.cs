9b584b52546cd88a78e16380daf9f2850f52f3c4
Sean T. McBeth <smcbeth@dlsdc.com>
2022-08-24 16:00:32 -0400

    cleanup build

---
+++ src/Juniper.Root/TSBuild/BuildSystem.cs
@@ -0,0 +1,18 @@
+namespace Juniper.TSBuild
+{
+    public struct BuildSystemDependency
+    {
+        public string Name { get; set; }
+        public string[] From { get; set; }
+        public string[] To { get; set; }
+    }
+
+    public class BuildSystemOptions
+    {
+        public string InProjectName;
+        public string OutProjectName;
+        public Dictionary<string, (FileInfo From, FileInfo To)> Dependencies;
+        public Dictionary<string, (FileInfo From, FileInfo To)> OptionalDependencies;
+        public (string Name, string Version, string Reason)[] BannedDependencies;
+    }
+}
--- src/Juniper.TSBuild/BuildSystem.cs
+++ src/Juniper.TSBuild/BuildSystem.cs
@@ -12,22 +12,6 @@ namespace Juniper.TSBuild
         { }
     }
 
-    public struct BuildSystemDependency
-    {
-        public string Name { get; set; }
-        public string[] From { get; set; }
-        public string[] To { get; set; }
-    }
-
-    public class BuildSystemOptions
-    {
-        public string? InProjectName;
-        public string? OutProjectName;
-        public Dictionary<string, (FileInfo From, FileInfo To)>? Dependencies;
-        public Dictionary<string, (FileInfo From, FileInfo To)>? OptionalDependencies;
-        public (string Name, string Version, string Reason)[]? BannedDependencies;
-    }
-
     public class BuildSystem : ILoggingSource, IDisposable
     {
         delegate void Writer(string format, params object[] args);
