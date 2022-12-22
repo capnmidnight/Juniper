caaa6570f71df70aba1517ab51ad9c251842666a
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-12-22 12:45:17 -0500

    method says it returns a URL, so make it return a URL

--- src/Juniper.TypeScript/@juniper-lib/dom/blobToDataURL.ts
+++ src/Juniper.TypeScript/@juniper-lib/dom/blobToDataURL.ts
@@ -1,9 +1,9 @@
 import { Task } from "@juniper-lib/tslib/events/Task";
 
-export function blobToDataURL(blob: Blob): Promise<string> {
-    const task = new Task<string>();
+export function blobToDataURL(blob: Blob): Promise<URL> {
+    const task = new Task<URL>();
     const reader = new FileReader();
-    reader.onload = _e => task.resolve(reader.result as string);
+    reader.onload = _e => task.resolve(new URL(reader.result as string));
     reader.onerror = _e => task.reject(reader.error);
     reader.onabort = _e => task.reject(new Error("Read aborted"));
     reader.readAsDataURL(blob);
