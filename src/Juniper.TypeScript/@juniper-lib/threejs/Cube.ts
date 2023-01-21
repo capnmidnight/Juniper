e9dc8dbca091b1c5b5919de60e5e5897ddadb061
Sean T. McBeth <sean.mcbeth@gmail.com>
2023-01-21 10:26:12 -0500

    cleanup cube code

--- src/Juniper.TypeScript/@juniper-lib/threejs/Cube.ts
+++ src/Juniper.TypeScript/@juniper-lib/threejs/Cube.ts
@@ -2,18 +2,18 @@ import { BoxGeometry, Mesh } from "three";
 import type { SolidMaterial } from "./materials";
 import { setGeometryUVsForCubemaps } from "./setGeometryUVsForCubemaps";
 
-export const geom = /*@__PURE__*/ new BoxGeometry(1, 1, 1, 1, 1, 1);
-geom.name = "CubeGeom";
-geom.computeBoundingBox();
-geom.computeBoundingSphere();
+export const cubeGeom = /*@__PURE__*/ new BoxGeometry(1, 1, 1, 1, 1, 1);
+cubeGeom.name = "CubeGeom";
+cubeGeom.computeBoundingBox();
+cubeGeom.computeBoundingSphere();
 
-export const invCube = /*@__PURE__*/ geom.clone() as BoxGeometry;
-invCube.name = "InvertedCubeGeom";
-setGeometryUVsForCubemaps(invCube);
+export const invCubeGeom = /*@__PURE__*/ cubeGeom.clone() as BoxGeometry;
+invCubeGeom.name = "InvertedCubeGeom";
+setGeometryUVsForCubemaps(invCubeGeom);
 
 export class Cube extends Mesh<BoxGeometry, SolidMaterial>{
     constructor(sx: number, sy: number, sz: number, material: SolidMaterial) {
-        super(geom, material);
+        super(cubeGeom, material);
         this.scale.set(sx, sy, sz);
     }
 }
@@ -24,9 +24,15 @@ export function cube(name: string, sx: number, sy: number, sz: number, material:
     return c;
 }
 
-export class InvCube extends Mesh {
+export class InvCube extends Mesh<BoxGeometry, SolidMaterial> {
     constructor(sx: number, sy: number, sz: number, material: SolidMaterial) {
-        super(invCube, material);
+        super(invCubeGeom, material);
         this.scale.set(sx, sy, sz);
     }
+}
+
+export function invCube(name: string, sx: number, sy: number, sz: number, material: SolidMaterial) {
+    const c = new InvCube(sx, sy, sz, material);
+    c.name = name;
+    return c;
 }
