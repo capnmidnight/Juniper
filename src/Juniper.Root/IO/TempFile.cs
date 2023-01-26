bc4ed29367074067a189c81a06952b2db9df82b1
Sean T. McBeth <sean.mcbeth@gmail.com>
2023-01-26 02:03:08 -0500

    create temp file targets that delete themselves

---
+++ src/Juniper.Root/IO/TempFile.cs
@@ -0,0 +1,50 @@
+namespace Juniper.IO
+{
+    public class TempFile : IDisposable
+    {
+        private bool disposedValue;
+
+        public string FilePath { get; }
+
+        public TempFile(MediaType mediaType = null)
+        {
+            FilePath = Path.GetTempFileName();
+            if(mediaType is not null)
+            {
+                FilePath = mediaType.AddExtension(FilePath);
+            }
+        }
+
+        public async Task WriteAsync(Stream stream)
+        {
+            using var fileStream = new FileStream(FilePath, FileMode.Create, FileAccess.Write);
+            await stream.CopyToAsync(fileStream);
+            await fileStream.FlushAsync();
+        }
+
+        public Stream Open()
+        {
+            return File.OpenRead(FilePath);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    File.Delete(FilePath);
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
