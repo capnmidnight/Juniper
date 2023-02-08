fef4e9f63fb98d4f1009f91076fb1e0b1ae31175
Sean T. McBeth <sean.mcbeth@gmail.com>
2023-02-08 13:27:55 -0500

    send email to admin when user requests password reset

--- src/Juniper.Server/EntityFramework/Identity/UserManagerExt.cs
+++ src/Juniper.Server/EntityFramework/Identity/UserManagerExt.cs
@@ -1,3 +1,6 @@
+using Juniper.Services;
+using static Juniper.Services.EmailMessage;
+
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -10,19 +13,21 @@ namespace Juniper.EntityFramework.Identity
 {
     public static class UserManagerExt
     {
-        public static async Task<IdentityResult> SendPasswordChangeEmail(
+        public static async Task<IdentityResult> SendPasswordChangeEmailAsync(
             this UserManager<IdentityUser> users,
             HttpRequest request,
-            IUrlHelper urls,
+            ILogger logger,
+            IUrlHelper Url,
             IEmailSender email,
             IdentityUser user,
+            string adminEmail,
             string siteName,
-            string siteUrl,
-            string salutation)
+            string salutation,
+            bool send)
         {
             try
             {
-                if(user.Email is null)
+                if (user?.Email is null)
                 {
                     throw new ArgumentNullException("user.Email");
                 }
@@ -34,44 +39,72 @@ namespace Juniper.EntityFramework.Identity
                     .AddDays(1)
                     .ToLocalTime()
                     .ToString(CultureInfo.CurrentCulture);
-                var callbackUrl = urls.Page(
-                    "/Account/ResetPassword",
-                    pageHandler: null,
-                    values: new { area = "Identity", code, email = user.Email },
-                    protocol: request.Scheme);
-                var subject = hasPassword ? $"Reset Your {siteName} Password" : $"Welcome to {siteName}";
+
+                var callbackUrl = Url.Page("/Account/ResetPassword", new { area = "Identity", code, email = user.Email });
+                if (callbackUrl is null)
+                {
+                    throw new ArgumentNullException(nameof(callbackUrl));
+                }
+
+                var userName = user.UserName ?? user.Email;
+
+
+                var siteUrl = $"{request.Scheme}://{request.Host}";
+                var userProfileUrl = $"{siteUrl}/Editor/Users/Detail/{user.Id}";
+                callbackUrl = $"{siteUrl}{callbackUrl}";
+
+                siteUrl = A(siteUrl);
+                userProfileUrl = A(userProfileUrl, userName);
+                callbackUrl = A(callbackUrl);
+
+                var subjectToUser = hasPassword ? $"Reset Your {siteName} Password" : $"Welcome to {siteName}";
+                var subjectToAdmin = hasPassword ? $"Reseting {siteName} Password for {user.UserName}" : $"Requesting new {siteName} password for {user.UserName}";
                 var title = hasPassword ? "Reset" : "Set";
-                var message = new StringBuilder($"<h1>{title} Your {siteName} Password</h1>\n");
+                var toUser = new EmailMessage(user.Email, subjectToUser);
+                toUser += H1($"{title} Your {siteName} Password");
+
+                var toAdmin = new EmailMessage(adminEmail, subjectToAdmin);
+                toAdmin += H1($"User {user.UserName} must {title} their {siteName} Password");
+
                 if (hasPassword)
                 {
-                    message.AppendLine($@"<p>
-    We received a request from you to reset your <a href=""{siteUrl}"">{siteUrl}</a> password.
-    Please use the following link to visit a secure Web page where you may set a new password.
-    This link will expire on {expiry}.
-</p>");
+                    toUser += P($@"We received a request from you to reset your {siteUrl} password.
+Please use the following link to visit a secure Web page where you may set a new password.
+This link will expire on {expiry}.");
+
+                    toAdmin += P($@"We sent a request to {userProfileUrl} to reset their {siteUrl} password.
+They have until {expiry} to do so.");
 
                 }
                 else
                 {
-                    message.AppendLine($@"<p>
-    We have created a new account for you at <a href=""https://{siteUrl}"">{siteUrl}</a>.
-    Please use the following link to visit a secure Web page where you may set a password for your account.
-    This link will expire on {expiry}.
-</p>");
+                    toUser += P($@"We have created a new account for you at {siteUrl}.
+Please use the following link to visit a secure Web page where you may set a password for your account.
+This link will expire on {expiry}.");
+                    toAdmin += P($@"We created a new account for {userProfileUrl} at {siteUrl}.
+They have until {expiry} to set their password.");
                 }
 
-                message.AppendLine($@"<p>
-    <a href=""{callbackUrl}"">{callbackUrl}</a>
-</p>");
+                toUser += P(callbackUrl);
 
                 if (hasPassword)
                 {
-                    message.AppendLine("<p>If you did not request a password reset, you may ignore this email.</p>");
+                    toUser += P("If you did not request a password reset, you may ignore this email.");
                 }
 
-                message.AppendLine($"<p>Thank you<br/>- <em>{salutation}</em></p>");
+                toUser += Thanks(salutation);
+                toAdmin += Thanks(salutation);
 
-                await email.SendEmailAsync(user.Email, subject, message.ToString());
+                if (send)
+                {
+                    await (toUser >> email);
+                    await (toAdmin >> email);
+                }
+                else
+                {
+                    logger.LogInformation("Message sent to user: {toUser}", toUser);
+                    logger.LogInformation("Message sent to admin: {toAdmin}", toAdmin);
+                }
 
                 return IdentityResult.Success;
             }
