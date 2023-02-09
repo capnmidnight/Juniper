dee9927c099983edd2357eff1dfc4da803eaae4c
Sean T. McBeth <sean.mcbeth@gmail.com>
2023-02-08 22:43:29 -0500

    config

--- src/Juniper.Server/Configuration/IConfigurationExt.Azure.cs
+++ src/Juniper.Server/Configuration/IConfigurationExt.Azure.cs
@@ -4,10 +4,16 @@ namespace Juniper.Configuration
     {
         public static string? GetAzureSubscriptionKey(this IConfiguration config) =>
             config.GetSection("Azure")
+                .GetSection("Speech")
                 .GetValue<string>("SubscriptionKey");
 
         public static string? GetAzureRegion(this IConfiguration config) =>
             config.GetSection("Azure")
+                .GetSection("Speech")
                 .GetValue<string>("Region");
+
+        public static string? GetOpenAIKey(this IConfiguration config) =>
+            config.GetSection("OpenAI")
+                .GetValue<string>("APIKey");
     }
 }
