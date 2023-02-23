291a6eec03872b15706e8474baaa266aaabcc121
Sean T. McBeth <sean.mcbeth@gmail.com>
2023-02-22 20:49:06 -0500

    fix hax

--- src/Juniper.TypeScript/@juniper-lib/hax/haxFunction.ts
+++ src/Juniper.TypeScript/@juniper-lib/hax/haxFunction.ts
@@ -1,14 +1,15 @@
 import { copyProps } from "./copyProps";
 import { Hax } from "./Hax";
 
-export function haxMethod<T, K extends keyof T & string, V extends T[K] & Function>(obj: T, method: V, name: K, hax: V, obj2: any = null) {
+
+export function haxFunction<T, K extends keyof T & string, V extends T[K] & Function>(obj: T, method: V, name: K, hax: V) {
     if (method != obj[name]) {
         throw new Error(`The provided method is not the same object as the field "${name}" in the provided object.`);
     }
 
-    (obj as any)[name] = function (...params: any[]) {
-        hax.apply(obj2, params);
-        return method.apply(obj, params);
+    (obj as any)[name] = function(...params: any[]) {
+        hax(...params);
+        return method.apply(this, params);
     };
 
     copyProps(method, obj[name]);
