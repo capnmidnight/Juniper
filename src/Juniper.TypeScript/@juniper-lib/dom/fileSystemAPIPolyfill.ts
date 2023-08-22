f5a02b8f2f2379ce4bb512bfc8f1ba5a9700a46c
Sean T. McBeth <sean.mcbeth@gmail.com>
2023-08-21 21:46:38 -0400

    cleanup file system polyfill

--- src/Juniper.TypeScript/@juniper-lib/dom/fileSystemAPIPolyfill.ts
+++ src/Juniper.TypeScript/@juniper-lib/dom/fileSystemAPIPolyfill.ts
@@ -1,30 +1,27 @@
-﻿import { Accept, Target } from "@juniper-lib/dom/attrs";
+﻿import { Target } from "@juniper-lib/dom/attrs";
 import { A, InputFile } from "@juniper-lib/dom/tags";
 import { once } from "@juniper-lib/events/once";
-import { Image_SvgXml } from "@juniper-lib/mediatypes";
 import { identity } from "@juniper-lib/tslib/identity";
 
 if (!("showOpenFilePicker" in globalThis)) {
-    const fileInput = InputFile(
-        Accept(Image_SvgXml.value)
-    );
+    const fileInput = InputFile();
 
     const anchor = A(Target("_blank"));
 
     Object.assign(globalThis, {
+
         async showOpenFilePicker(options?: OpenFilePickerOptions) {
             fileInput.multiple = options && options.multiple;
-            if (options && options.types) {
-                fileInput.accept = options
-                    .types
-                    .filter(v => v.accept)
-                    .flatMap(v => Object.keys(v.accept))
-                    .join(",");
-            }
+            fileInput.accept = options && options.types
+                .filter(v => v.accept)
+                .flatMap(v => Object.keys(v.accept))
+                .join(",");
+
             const task = once(fileInput, "input", "cancel")
                 .catch(() => {
                     throw new DOMException("The user aborted a request.");
                 });
+
             fileInput.click();
 
             await task;
@@ -32,23 +29,26 @@ if (!("showOpenFilePicker" in globalThis)) {
             return Array.from(fileInput.files)
                 .map(file => {
                     return {
+
                         getFile() {
                             return Promise.resolve(file);
                         }
+
                     };
                 });
         },
 
-        async showSaveFilePicker(options?: SaveFilePickerOptions) {
-            anchor.download = options && options.suggestedName || null;
-            let blobParts = new Array<BlobPart>();
-            return {
-                async createWritable() {
-                    return {
-                        async write(blob: BlobPart) {
+        showSaveFilePicker(options?: SaveFilePickerOptions) {
+            const blobParts = new Array<BlobPart>();
+            return Promise.resolve({
+                createWritable() {
+                    return Promise.resolve({
+
+                        write(blob: BlobPart) {
                             blobParts.push(blob);
                             return Promise.resolve();
                         },
+
                         async close() {
                             const types = options
                                 && options.types
@@ -59,13 +59,15 @@ if (!("showOpenFilePicker" in globalThis)) {
                             const blob = new Blob(blobParts, { type });
                             const url = URL.createObjectURL(blob);
                             const task = once(anchor, "click");
+                            anchor.download = options && options.suggestedName || null;
                             anchor.href = url;
                             anchor.click();
                             await task;
                         }
-                    };
+
+                    });
                 }
-            };
+            });
         }
     });
 }
