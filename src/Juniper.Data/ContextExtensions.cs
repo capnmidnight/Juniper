b610e040c0e3dc387b079e563213c10746ec123b
Sean McBeth <sean.mcbeth@spa.com>
2023-09-25 16:54:23 -0400

    More entity framework helpers

---
+++ src/Juniper.Data/ContextExtensions.cs
@@ -0,0 +1,77 @@
+﻿// Ignore Spelling: Sql
+
+using Microsoft.EntityFrameworkCore;
+
+using System.Reflection;
+
+namespace Juniper.Data;
+
+public static class ContextExtensions
+{
+    public static void ApplyModelConfigurators<ContextT>(this ModelBuilder builder)
+        where ContextT : DbContext
+    {
+        foreach (var configurator
+            in from type in typeof(ContextT).Assembly.GetTypes()
+               let typeName = type.Name
+               from method in type.GetMethods(BindingFlags.Public | BindingFlags.Static)
+               let parameters = method.GetParameters()
+               where parameters.FirstOrDefault()?.ParameterType == typeof(ModelBuilder)
+               select method)
+        {
+            configurator.Invoke(null, new[] { builder });
+        }
+    }
+
+    public static void ApplyTableAttributes<ContextT>(this ModelBuilder builder)
+        where ContextT : DbContext
+    {
+        foreach (var (type, attrs)
+            in from type in typeof(ContextT).Assembly.GetTypes()
+               let attrs = type.GetCustomAttributes().ToArray()
+               where attrs.Length > 0
+               select (type, attrs))
+        {
+            foreach (var attr in attrs)
+            {
+                if (attr is AlternateKeyAttribute unique)
+                {
+                    builder
+                        .Entity(type)
+                        .HasAlternateKey(unique.PropertyNames);
+                }
+            }
+        }
+    }
+
+    public static void ApplyColumnAttributes<ContextT>(this ModelBuilder builder)
+        where ContextT : DbContext
+    {
+        foreach (var (type, prop, attrs)
+            in from type in typeof(ContextT).Assembly.GetTypes()
+               from prop in type.GetProperties()
+               let attrs = prop.GetCustomAttributes().ToArray()
+               where attrs.Length > 0
+               select (type, prop, attrs))
+        {
+            foreach (var attr in attrs)
+            {
+                if (attr is DefaultValueSqlAttribute defValue)
+                {
+                    builder
+                        .Entity(type)
+                        .Property(prop.Name)
+                        .HasDefaultValueSql(defValue.Value);
+                }
+            }
+        }
+    }
+
+    public static void JuniperModelCreating<ContextT>(this ModelBuilder builder)
+        where ContextT : DbContext
+    {
+        builder.ApplyModelConfigurators<ContextT>();
+        builder.ApplyTableAttributes<ContextT>();
+        builder.ApplyColumnAttributes<ContextT>();
+    }
+}
--- src/Juniper.Data/DefaultValueSqlAttribute.cs
+++ src/Juniper.Data/DefaultValueSqlAttribute.cs
@@ -1,11 +1,5 @@
 ﻿// Ignore Spelling: Sql
 
-using Microsoft.EntityFrameworkCore;
-
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Runtime.CompilerServices;
-
 namespace Juniper.Data;
 
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
@@ -16,47 +10,5 @@ public class DefaultValueSqlAttribute : Attribute
     public DefaultValueSqlAttribute(string value)
     {
         Value = value;
-    }
-}
-
-public static class ContextExtensions
-{
-    public static void ApplyModelConfigurators<ContextT>(this ContextT db, ModelBuilder builder)
-        where ContextT : DbContext
-    {
-        foreach (var configurator
-            in from type in typeof(ContextT).Assembly.GetTypes()
-               let typeName = type.Name
-               from method in type.GetMethods(BindingFlags.Public | BindingFlags.Static)
-               let parameters = method.GetParameters()
-               where parameters.FirstOrDefault()?.ParameterType == typeof(ModelBuilder)
-               select method)
-        {
-            configurator.Invoke(null, new[] { builder });
-        }
-    }
-
-    public static void ApplyDefaultSqlValues<ContextT>(this ContextT db, ModelBuilder builder)
-        where ContextT : DbContext
-    {
-        foreach (var (type, prop, attr)
-            in from type in typeof(ContextT).Assembly.GetTypes()
-               from prop in type.GetProperties()
-               let attr = prop.GetCustomAttribute<DefaultValueSqlAttribute>()
-               where attr is not null
-               select (type, prop, attr))
-        {
-            builder
-                .Entity(type)
-                .Property(prop.Name)
-                .HasDefaultValueSql(attr.Value);
-        }
-    }
-
-    public static void JuniperModelCreating<ContextT>(this ContextT db, ModelBuilder builder)
-        where ContextT : DbContext
-    {
-        db.ApplyModelConfigurators(builder);
-        db.ApplyDefaultSqlValues(builder);
     }
 }
