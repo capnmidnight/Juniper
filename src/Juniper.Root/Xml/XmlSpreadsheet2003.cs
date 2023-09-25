7185da4b592861c76ec4257547772dbfb9183a62
Sean McBeth <sean.mcbeth@spa.com>
2023-09-25 09:09:08 -0400

    XmlSpreadsheet2003 cleanup

--- src/Juniper.Root/Xml/XmlSpreadsheet2003.cs
+++ src/Juniper.Root/Xml/XmlSpreadsheet2003.cs
@@ -2,16 +2,6 @@
 
 namespace Juniper.Xml
 {
-    public static class XmlSpreadsheet2003Extensions
-    {
-        public static XmlSpreadsheet2003.Table Union(this IEnumerable<XmlSpreadsheet2003.Table> table)
-        {
-            var first = table.First();
-            var rest = table.Skip(1).All(t => t.IsMatch(first.Headers));
-            return new XmlSpreadsheet2003.Table(table.SelectMany(t => t.Rows));
-        }
-    }
-
     public class XmlSpreadsheet2003
     {
         private static readonly XName WORKSHEET = XName.Get("Worksheet", "urn:schemas-microsoft-com:office:spreadsheet");
@@ -58,29 +48,14 @@ namespace Juniper.Xml
 
         public class Table
         {
-            public string[] Headers { get; }
+            public HashSet<string> Headers { get; }
             public List<Row> Rows { get; }
 
-            public string SortKey => Headers
+            private string sortKey = null;
+            public string SortKey => sortKey ??= Headers
+                .OrderBy(Always.Identity)
                 .ToArray()
                 .Join(", ");
-
-            public bool IsMatch(params string[] headerPattern)
-            {
-                var temp = headerPattern.ToArray();
-                Array.Sort(temp);
-                return temp.Length == Headers.Length
-                    && temp
-                        .Select((v, i) => v == Headers[i])
-                        .All(v => v);
-            }
-
-            internal Table(IEnumerable<Row> rows)
-            {
-                Headers = rows.SelectMany(r => r.Headers).Distinct().ToArray();
-                Array.Sort(Headers);
-                Rows = rows.ToList();
-            }
 
             public bool IsUnderused => Rows.Any(r => r.IsUnderused);
 
@@ -90,20 +65,21 @@ namespace Juniper.Xml
             {
                 if (element is null)
                 {
-                    throw new Exception("Excpected a Table element");
+                    throw new Exception("Expected a Table element");
                 }
 
                 var rows = new Queue<XElement>(element.Elements(ROW));
-                Headers = (from cell in rows.Dequeue().Elements(CELL)
+                var headers = (from cell in rows.Dequeue().Elements(CELL)
                            let header = cell.Element(DATA)
                            select header.Value)
                             .ToArray();
 
+                Headers = new(headers);
                 Rows = new List<Row>();
                 foreach (var row in rows)
                 {
                     var dict = new Dictionary<string, string>();
-                    foreach (var header in Headers)
+                    foreach (var header in headers)
                     {
                         dict.Add(header, "");
                     }
@@ -117,17 +93,23 @@ namespace Juniper.Xml
                             index = skipIndex - 1;
                         }
 
-                        if (0 <= index && index < Headers.Length)
+                        if (0 <= index && index < headers.Length)
                         {
                             var val = cell.Element(DATA)?.Value ?? "";
-                            dict[Headers[index]] = val;
+                            dict[headers[index]] = val;
                             ++index;
                         }
                     }
                 }
 
-                Array.Sort(Headers);
             }
+
+            public bool Has(params string[] headers) =>
+                headers.All(Headers.Contains);
+
+            public bool Matches(params string[] headers) =>
+                headers.Length == Headers.Count
+                && Has(headers);
         }
 
         public class Row
