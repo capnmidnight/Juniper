3f201fae86f3c8a0e374c65de0f86e136cca7fb8
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2023-10-21 11:30:27 -0400

    A little cleaner

--- src/Juniper.AppShell.GTK/GtkAppShellFactory.cs
+++ src/Juniper.AppShell.GTK/GtkAppShellFactory.cs
@@ -5,22 +5,25 @@ using Gtk;
 public class GtkAppShellFactory<AppShellT> : IAppShellFactory
     where AppShellT : Window, IAppShell, new()
 {
-    private readonly TaskCompletionSource<IAppShell> appShellCreating = new TaskCompletionSource<IAppShell>();
     public Task<IAppShell> StartAsync(CancellationToken cancellationToken)
     {
-        return appShellCreating.Task;
-    }
-
-    public void Run()
-    {
         Application.Init();
+
         var app = new Application("org.juniper.appshell", GLib.ApplicationFlags.None);
-        var appShell = new AppShellT();
         app.Register(GLib.Cancellable.Current);
+
+        var appShell = new AppShellT();
+        appShell.WaitForCloseAsync().ContinueWith(delegate {
+            Application.Invoke(delegate {
+                Application.Quit();
+            });
+        }, cancellationToken);
+
         app.AddWindow(appShell);
+
         appShell.ShowAll();
-        appShellCreating.SetResult(appShell);
-        Application.Run();
+
+        return Task.FromResult((IAppShell)appShell);
     }
 }
 
