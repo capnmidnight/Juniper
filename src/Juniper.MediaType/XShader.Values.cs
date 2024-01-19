98ac59c3f22f5fe704d6a74d77c3376536008d11
Sean McBeth <sean.mcbeth@spa.com>
2024-01-19 14:54:09 -0500

    more shader MIME types

--- src/Juniper.MediaType/XShader.Values.cs
+++ src/Juniper.MediaType/XShader.Values.cs
@@ -2,7 +2,9 @@ namespace Juniper
 {
     public partial class MediaType
     {
-        public static readonly XShader XShader_XVertex = new("x-vertex", "vert", "glsl");
-        public static readonly XShader XShader_XFragment = new("x-fragment", "frag", "glsl");
+        public static readonly XShader XShader_XVertex = new("x-vertex", "vert", "vs");
+        public static readonly XShader XShader_XFragment = new("x-fragment", "frag", "fs");
+        public static readonly XShader XShader_XGLSL = new("x-glsl", "glsl");
+        public static readonly XShader XShader_XWGSL = new("x-wgsl", "wgsl");
     }
 }
--- src/Juniper.TypeScript/@juniper-lib/mediatypes/dist/xShader.d.ts
+++ src/Juniper.TypeScript/@juniper-lib/mediatypes/dist/xShader.d.ts
@@ -1,3 +1,5 @@
 export declare const XShader_XVertex: import("./util").MediaType;
 export declare const XShader_XFragment: import("./util").MediaType;
+export declare const XShader_XGLSL: import("./util").MediaType;
+export declare const XShader_XWGSL: import("./util").MediaType;
 //# sourceMappingURL=xShader.d.ts.map
