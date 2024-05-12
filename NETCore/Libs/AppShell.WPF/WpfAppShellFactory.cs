7407c1480998921adbc53f657e8c1f6de8859e07
Sean T. McBeth <sean.mcbeth@gmail.com>
2024-05-12 11:14:17 -0400

    reorgs

---
+++ NETCore/Libs/AppShell.WPF/WpfAppShellFactory.cs
@@ -0,0 +1,42 @@
+﻿#if WINDOWS
+using System.Windows;
+#endif
+
+namespace Juniper.AppShell;
+
+
+public class WpfAppShellFactory<AppShellT> : IAppShellFactory
+    where AppShellT : 
+    #if WINDOWS
+    Window, 
+    #endif
+    IAppShell, new()
+{
+    public Task<IAppShell> StartAsync(CancellationToken cancellationToken)
+    {
+        #if WINDOWS
+        var appShellCreating = new TaskCompletionSource<IAppShell>();
+        var thread = new Thread(() =>
+        {
+            var app = Application.Current ?? new Application();
+            app.Dispatcher.Invoke(() =>
+            {
+                var appShell = new AppShellT();
+                appShellCreating.TrySetResult(appShell);
+                app.Run(appShell);
+            });
+        });
+
+        thread.SetApartmentState(ApartmentState.STA);
+        thread.Start();
+
+        return appShellCreating.Task;
+        #else
+        throw new PlatformNotSupportedException("WPF AppShell is only available on Windows.");
+        #endif
+    }
+}
+
+public class WpfAppShellFactory : WpfAppShellFactory<WpfAppShell> {
+    
+}
